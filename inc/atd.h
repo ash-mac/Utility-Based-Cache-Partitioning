88505c75cdcda2a62df5c7052170987e3b7e01e7
Ashish Sharma <60055422+ash-mac@users.noreply.github.com>
2022-06-27 00:18:00 +0530

    initial commit

---
+++ inc/atd.h
@@ -0,0 +1,35 @@
+// G9: This class defines block present in an atd and ATD itself
+#include <cache.h>
+class blockatd
+{
+public:
+       int lru, valid, tag;
+
+       blockatd()
+       {
+              lru = 0;
+              valid = 0;
+              tag = 0;
+       }
+};
+
+class ATD
+{
+
+public:
+       int nsets = 64;
+       blockatd arr[64][16];
+       int umonglobalcounter[16] = {0};
+
+       int getumon(int n)
+       {
+              long long sum = 0;
+              for (int i = 0; i < n; i++)
+              {
+                     sum += umonglobalcounter[i];
+              }
+              return sum;
+       }
+
+       // queues
+};
---
+++ obj/src/cache.d
@@ -0,0 +1,19 @@
+obj/src/cache.o: src/cache.cc inc/cache.h inc/memory_class.h \
+ inc/champsim.h inc/block.h inc/instruction.h inc/set.h inc/set.h \
+ inc/atd.h
+
+inc/cache.h:
+
+inc/memory_class.h:
+
+inc/champsim.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/set.h:
+
+inc/set.h:
+
+inc/atd.h:
